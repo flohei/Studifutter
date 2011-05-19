fac6fbcff75bb465dc3e63767191549f632c5fbf
Florian Heiber <fh@rtfnt.com>
2011-05-19 12:19:18 +0000

    initial import
    git-svn-id: http://svn.rtfnt.com/svn/studifutter/dev/tags/studifutter-0.3@1 dd375700-57de-49ca-a908-404e403163fd

---
+++ Classes/Mensa.m
@@ -0,0 +1,24 @@
+//
+//  Mensa.m
+//  Mensa
+//
+//  Created by Florian Heiber on 07.04.09.
+//  Copyright 2009 rootof.net Florian Heiber & Daniel Wiewel GbR. All rights reserved.
+//
+
+#import "Mensa.h"
+
+
+@implementation Mensa
+@synthesize latitude, longitude, name, street, zipcode, city, googleMapsURL;
+
+- (void)dealloc {
+	[name release];
+	[street release];
+	[zipcode release];
+	[city release];
+	[googleMapsURL release];
+	[super dealloc];
+}
+
+@end
