3dd2a22e76355c38396f75fd4ca6c40dda595dbf
Florian Heiber <fh@rtfnt.com>
2011-10-31 18:57:32 +0000

    - added basic classes - added german localization files
    git-svn-id: http://svn.rtfnt.com/svn/studifutter/dev/trunk/app@12 dd375700-57de-49ca-a908-404e403163fd

---
+++ Studifutter/SFRestaurantViewController.h
@@ -0,0 +1,15 @@
+//
+//  SFRestaurantViewController.h
+//  Studifutter
+//
+//  Created by Florian Heiber on 31.10.11.
+//  Copyright (c) 2011 rootof.net Heiber & Wiewel GbR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFRestaurantViewController : UITableViewController
+
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+
+@end
