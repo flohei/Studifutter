4a3c666a193f1022f55481f1775c6bbc44f55a84
Florian Heiber <fh@rtfnt.com>
2011-11-09 19:47:38 +0000

    added allMenus data source object
    git-svn-id: http://svn.rtfnt.com/svn/studifutter/dev/trunk@37 dd375700-57de-49ca-a908-404e403163fd

--- app/Studifutter/SFDayListViewController.m
+++ app/Studifutter/SFDayListViewController.m
@@ -7,7 +7,17 @@
 //
 
 #import "SFDayListViewController.h"
+#import "Restaurant.h"
+#import "MenuSet.h"
+#import "Menu.h"
+#import "DayTableViewCell.h"
+#import "SFMenuViewController.h"
 
+@interface SFDayListViewController ()
+
+- (NSArray *)allMenus;
+
+@end
 
 @implementation SFDayListViewController
 
@@ -52,6 +62,20 @@
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
+#pragma mark - Misc
+
+- (NSArray *)allMenus {
+    NSSortDescriptor *dateSortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"date" ascending:YES];
+    return [[[self restaurant] menuSet] sortedArrayUsingDescriptors:[NSArray arrayWithObject:dateSortDescriptor]];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([[segue identifier] isEqualToString:@"DayList"]) {
+        SFMenuViewController *menuViewController = (SFMenuViewController *)[segue destinationViewController];
+        menuViewController.menuSet = [(DayTableViewCell *)sender menuSet];
+    }
+}
+
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
@@ -63,34 +87,23 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    return 0;
+    return [[self allMenus] count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
+    static NSString *CellIdentifier = @"DayCellIdentifier";
     
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    DayTableViewCell *cell = (DayTableViewCell *)[tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        cell = [[DayTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
     }
     
     // Configure the cell...
+    MenuSet *menuSet = [[self allMenus] objectAtIndex:[indexPath row]];
+    cell.menuSet = menuSet;
     
     return cell;
-}
-
-#pragma mark - Table view delegate
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
 }
 
 @end
