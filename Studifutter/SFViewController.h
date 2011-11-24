d343949bc0df9492c2d114c7652323ed2450017f
Florian Heiber <fh@rtfnt.com>
2011-11-24 15:23:24 +0000

    - cleanup graphics - adding SFViewController and SFTableViewController
    git-svn-id: http://svn.rtfnt.com/svn/studifutter/dev/trunk/app@90 dd375700-57de-49ca-a908-404e403163fd

---
+++ Studifutter/SFViewController.h
@@ -0,0 +1,13 @@
+//
+//  SFViewController.h
+//  Studifutter
+//
+//  Created by Florian Heiber on 24.11.11.
+//  Copyright (c) 2011 rootof.net Heiber & Wiewel GbR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFViewController : UIViewController
+
+@end
