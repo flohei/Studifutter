e75cb71fd241797c4e7373a693dc04aafb5b0025
Florian Heiber <fh@rtfnt.com>
2011-11-24 15:48:26 +0000

    cleanup
    git-svn-id: http://svn.rtfnt.com/svn/studifutter/dev/trunk/app@92 dd375700-57de-49ca-a908-404e403163fd

--- Studifutter/SFRestaurantViewController.m
+++ Studifutter/SFRestaurantViewController.m
@@ -19,34 +19,11 @@
 @synthesize managedObjectContext = _managedObjectContext;
 @synthesize fetchedResultsController = _fetchedResultsController;
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)didReceiveMemoryWarning
-{
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
-}
-
 #pragma mark - View lifecycle
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
     
     NSError *error;
 	if (![[self fetchedResultsController] performFetch:&error]) {
