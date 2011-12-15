22d61309eadc64b6da5befa856fffcb8468197b3
Florian Heiber <fh@rtfnt.com>
2011-12-15 19:04:56 +0000

    - added UISwipeGestureRecognizer - reconfigured the UITableView to be plain instead of grouped
    git-svn-id: http://svn.rtfnt.com/svn/studifutter/dev/trunk@98 dd375700-57de-49ca-a908-404e403163fd

--- app/Studifutter/SFMenuViewController.h
+++ app/Studifutter/SFMenuViewController.h
@@ -14,5 +14,6 @@
 @interface SFMenuViewController : SFTableViewController 
 
 @property (nonatomic, retain) MenuSet *menuSet;
+@property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *swipeGestureRecognizer;
 
 @end
