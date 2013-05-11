0f377cad8080647a97f607d2b1b8218249501111
Florian Heiber <fh@rtfnt.com>
2013-05-11 17:17:55 +0000

    cleanup for the new 2.1 tag
    git-svn-id: http://svn.rtfnt.com/svn/studifutter/dev/tags/studifutter-2.1@256 dd375700-57de-49ca-a908-404e403163fd

--- Studifutter/Human/Menu.h
+++
@@ -1,5 +0,0 @@
-#import "_Menu.h"
-
-@interface Menu : _Menu {}
-// Custom logic goes here.
-@end
--- Studifutter/Human/MenuSet.h
+++
@@ -1,9 +0,0 @@
-#import "_MenuSet.h"
-
-@interface MenuSet : _MenuSet {}
-
-- (NSString *)shareTextTwitter;
-- (NSString *)shareTextFacebook;
-- (NSString *)shareTextMail;
-
-@end
