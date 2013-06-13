f44918ba2efa9a993a09a6d47076f6d05a36e50b
Florian Heiber <fh@rtfnt.com>
2013-06-13 09:50:15 +0000

    updating build scripts
    git-svn-id: http://svn.rtfnt.com/svn/studifutter/dev/trunk@268 dd375700-57de-49ca-a908-404e403163fd

--- app/Scripts/update_managed_object_model.sh
+++ app/Scripts/update_managed_object_model.sh
@@ -1,1 +1,1 @@
-mogenerator -m ../Studifutter/Studifutter.xcdatamodeld/Studifutter.xcdatamodel -M ../Studifutter/Machine -H ../Studifutter/Human --template-var arc=true
+mogenerator -m ./Studifutter/Studifutter.xcdatamodeld/Studifutter.xcdatamodel -M ./Studifutter/Machine -H ./Studifutter/Human --template-var arc=true
