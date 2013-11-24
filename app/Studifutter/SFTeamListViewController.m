bf9d8db5221b4de72938c10db3aa915c3c85df90
Florian Heiber <fh@rtfnt.com>
2013-11-24 09:30:17 +0000

    closes #929
    git-svn-id: http://svn.rtfnt.com/svn/studifutter/dev/trunk@280 dd375700-57de-49ca-a908-404e403163fd

--- app/Studifutter/SFTeamListViewController.m
+++ app/Studifutter/SFTeamListViewController.m
@@ -30,4 +30,8 @@
     }
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
 @end
