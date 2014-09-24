7dc192e07ce0e929bb9aaa8df195e2b779444934
Florian Heiber <fh@rtfnt.com>
2014-09-24 16:41:57 +0200

    updating for iOS 8, adding app group

---
+++ app/StudifutterToday/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  StudifutterToday
+//
+//  Created by Florian Heiber on 24.09.14.
+//  Copyright (c) 2014 rootof.net Heiber & Wiewel GbR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
