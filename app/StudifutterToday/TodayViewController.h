4797b8d46d18008d71d155700453d0290d7a06de
Florian Heiber <fh@rtfnt.com>
2014-09-26 20:03:00 +0200

    preparing the today extension
    refs #1089

--- app/StudifutterToday/TodayViewController.h
+++ app/StudifutterToday/TodayViewController.h
@@ -7,7 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Restaurant.h"
 
-@interface TodayViewController : UIViewController
+@interface TodayViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, retain) Restaurant *restaurant;
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@property (nonatomic, retain) NSDictionary *sections;
+@property (nonatomic, retain) NSArray *sortedMonths;
 
 @end
--- app/StudifutterToday/TodayViewController.m
+++ app/StudifutterToday/TodayViewController.m
@@ -8,6 +8,9 @@
 
 #import "TodayViewController.h"
 #import <NotificationCenter/NotificationCenter.h>
+#import "FHCoreDataStack.h"
+#import "DayTableViewCell.h"
+#import "MenuSet.h"
 
 @interface TodayViewController () <NCWidgetProviding>
 
@@ -33,6 +36,123 @@
     // If there's an update, use NCUpdateResultNewData
 
     completionHandler(NCUpdateResultNewData);
+}
+
+- (NSDictionary *)sections {
+    if (!_sections) {
+        NSMutableDictionary *mutableSections = [[NSMutableDictionary alloc] init];
+        NSArray *allMenus = [self allMenus];
+        NSMutableArray *monthArray = [[NSMutableArray alloc] init];
+        
+        for (MenuSet *menuSet in allMenus) {
+            // get the month of the current menus date
+            NSString *monthStringForDate = [self monthStringForDate:[menuSet date]];
+            
+            // if we don't have an array for the current month yet go ahead and create one
+            NSMutableArray *menusInMonth = [mutableSections objectForKey:monthStringForDate];
+            if (!menusInMonth) {
+                menusInMonth = [[NSMutableArray alloc] init];
+                [mutableSections setObject:menusInMonth forKey:monthStringForDate];
+                [monthArray addObject:monthStringForDate];
+            }
+            
+            // add the menu
+            [menusInMonth addObject:menuSet];
+        }
+        
+        _sections = (NSDictionary *)[mutableSections copy];
+        mutableSections = nil;
+        
+        // create a list of sorted months
+        self.sortedMonths = (NSArray *)monthArray;
+    }
+    
+    return _sections;
+}
+
+- (NSArray *)allMenus {
+    NSSortDescriptor *dateSortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"date" ascending:YES];
+    NSSet *unsortedMenuSet = self.restaurant.menuSet;
+    
+    if ([unsortedMenuSet count] == 0) {
+        // show info that there's no data
+//        [[self view] addSubview:[self noDataAvailableLabel]];
+        [[self tableView] setHidden:YES];
+    } else {
+        // hide info that there's no data
+        [[self view] removeFromSuperview];
+        [[self tableView] setHidden:NO];
+    }
+    
+    if (unsortedMenuSet) {
+        return [unsortedMenuSet sortedArrayUsingDescriptors:[NSArray arrayWithObject:dateSortDescriptor]];
+    } else {
+        return nil;
+    }
+}
+
+- (NSString *)monthStringForDate:(NSDate *)date {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"MMMM"];
+    return [formatter stringFromDate:date];
+}
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return [[self sections] count];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSString *currentMonth = [[self sortedMonths] objectAtIndex:section];
+    NSArray *menusThatMonth = [[self sections] objectForKey:currentMonth];
+    return [menusThatMonth count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"DayCellIdentifier";
+    
+    DayTableViewCell *cell = (DayTableViewCell *)[tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[DayTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    // Configure the cell...
+    NSDate *currentMonth = [[self sortedMonths] objectAtIndex:[indexPath section]];
+    NSArray *menusThatMonth = [[self sections] objectForKey:currentMonth];
+    
+    MenuSet *menuSet = [menusThatMonth objectAtIndex:[indexPath row]];
+    cell.menuSet = menuSet;
+    
+    return cell;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    NSString *monthName = [self.sortedMonths objectAtIndex:section];
+    return monthName;
+}
+
+#pragma mark - Table view delegate
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 44;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    UIView *containerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 44)];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20, 0, 280, 44)];
+    NSString *title = [self tableView:tableView titleForHeaderInSection:section];
+    [label setText:title];
+    [label setFont:[UIFont boldSystemFontOfSize:17]];
+    [label setTextColor:[UIColor redColor]];
+    [label setBackgroundColor:[UIColor clearColor]];
+    
+    [containerView setBackgroundColor:[UIColor whiteColor]];
+    [containerView addSubview:label];
+    
+    return containerView;
 }
 
 @end
