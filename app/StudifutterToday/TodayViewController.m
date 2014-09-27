f11e55dc4ebff8c1fe97c16dada1afcf5aa7dc94
Florian Heiber <fh@rtfnt.com>
2014-09-27 12:47:53 +0200

    setting preferred content size

--- app/StudifutterToday/TodayViewController.m
+++ app/StudifutterToday/TodayViewController.m
@@ -21,6 +21,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    self.preferredContentSize = self.tableView.contentSize;
 }
 
 - (void)didReceiveMemoryWarning {
