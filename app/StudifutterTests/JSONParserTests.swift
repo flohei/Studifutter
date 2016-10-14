f181a03caeae4e91dadd44485045c993a58d2bf3
Florian Heiber <flo@flohei.de>
2016-10-14 22:54:29 +0200

    adding another basic test case

--- app/StudifutterTests/JSONParserTests.swift
+++ app/StudifutterTests/JSONParserTests.swift
@@ -11,10 +11,18 @@ import XCTest
 
 class JSONParserTests: XCTestCase {
     let parser = JSONParser()
+    let exampleJSONFileName = "mensateria"
+    var exampleJSON: JSON?
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        let bundle = Bundle(for: type(of: self))
+        guard let filePath = bundle.path(forResource: exampleJSONFileName, ofType: "json") else { return }
+        let fileURL = URL(fileURLWithPath: filePath)
+        let data = try? Data(contentsOf: fileURL)
+        exampleJSON = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! JSON
     }
     
     override func tearDown() {
@@ -42,9 +50,11 @@ class JSONParserTests: XCTestCase {
         XCTAssertTrue(date?.compare(compareDate!) == .orderedSame, "Dates don't match")
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCafeteriaParser() {
+        let cafeteria = parser.parseCafeteria(jsonObject: exampleJSON!)
+        
+        XCTAssertNotNil(cafeteria, "Cafeteria shouldn't be nil")
+        XCTAssertEqual(cafeteria?.description, "Mensa am Hauptcampus der TH Nürnberg")
     }
     
     func testPerformanceExample() {
@@ -53,5 +63,4 @@ class JSONParserTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-    
 }
